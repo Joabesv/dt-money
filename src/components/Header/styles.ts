baa054275bb7df377dc394ee2a7260fba68d28a1
Joabesv <joabevarjao123@gmail.com>
2023-01-05 01:33:42 -0300

    feat(Summary): styling Dtmoney Summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -10,15 +10,16 @@ export const HeaderContent = styled.div`
   max-width: 1120px;
   margin: 0 auto;
   padding: 0 1.5rem;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-export const newTransactionButton = styled.button`
+export const NewTransactionButton = styled.button`
   height: 50px;
   border: 0;
-  background: ${(props) => props.theme['gray-500']};
+  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
   font-weight: bold;
   padding: 0 1.25rem;
@@ -26,7 +27,7 @@ export const newTransactionButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background: ${(props) => props.theme['gray-700']};
+    background: ${(props) => props.theme['green-700']};
     transition: background-color 0.2s;
   }
 `;
