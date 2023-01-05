de698e56d8504fdde7b7eccbf97287d5e34191e2
Joabesv <joabevarjao123@gmail.com>
2023-01-05 00:00:51 -0300

    chore(vite) - delete older content and migrate to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,5 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
-import { GlobalStyle } from './styles/global';
-import { NewTransactionModal } from './components/NewTransactionModal/';
-import { TransactionsProvider } from './hooks/useTransactions';
-
-Modal.setAppElement('#root');
-
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
-    useState(false);
-
-  const toggleNewTransactionModal = () => {
-    setIsNewTransactionModalOpen(!isNewTransactionModalOpen);
-  };
-
   return (
-    <TransactionsProvider>
-      <Header onOpenNewTransactionModal={toggleNewTransactionModal} />
-
-      <Dashboard />
-
-      <NewTransactionModal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={toggleNewTransactionModal}
-      />
-
-      <GlobalStyle />
-    </TransactionsProvider>
-  );
+    <p>Hello World!</p>
+  )
 }
