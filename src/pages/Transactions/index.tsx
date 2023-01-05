369c1b2abd5f24fcdedd4cd9286eb9fae8fe00d7
Joabesv <joabevarjao123@gmail.com>
2023-01-05 01:55:46 -0300

    feat(Transactions): styling transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from './styles';
 
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Web Freelancing</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Rent</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.400,00</PriceHighlight>
+              </td>
+              <td>Monthly</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   );
 }
